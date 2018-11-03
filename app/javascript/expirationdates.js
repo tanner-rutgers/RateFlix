a53bddccb8743e68d0dd49f7e753061a95adf22c
Lemuel Lan <lemuellan@Lems-MacBook.local>
2018-11-02 23:38:50 -0700

    added pigeon

--- app/javascript/expirationdates.js
+++ app/javascript/expirationdates.js
@@ -1,9 +1,10 @@
 var movieTitles = ["The Mekong River with Sue Perkins",
-    "Goodbye Mr. Black", "Al’s Fish’n With Mates", "Dicte",
-    "Dreamland", "Lucha Underground", "Undercover Boss",
+    "Goodbye Mr. Black", "A Pigeon Sat on a Branch Reflecting on Existence", 
+    "Al’s Fish’n With Mates", "Dicte", "Dreamland", 
+    "Lucha Underground", "Undercover Boss",
     "Go Back to Where You Came From", "Goosebumps"];
 
-var expirationDates = ["Nov. 3rd", "Nov. 4th", "Nov. 5th", "Nov. 7th",
+var expirationDates = ["Nov. 3rd", "Nov. 4th", "Nov. 5th", "Nov. 5th", "Nov. 7th",
     "Nov. 10th", "Nov. 15th", "Nov. 17th", "Nov. 18th", "Nov. 19th"];
 
 function getTitles() {
