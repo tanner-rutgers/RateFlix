acc9db71fe2d4b8719aba20d20d39b0a3429d357
Huey Sun <hubert.s.sun@gmail.com>
2019-07-31 08:41:13 -0700

    cleaning up code

--- app/javascript/expirationdates.js
+++ app/javascript/expirationdates.js
@@ -3,68 +3,30 @@ var TIMEOUT = 3000;
 
 var expiredMovies = new Map();
 
-function getQueryUrl(){
+function getQueryUrl(skip=0){
   const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
 
   var today = new Date();
   var year = today.getFullYear();
-  var month = monthNames[today.getMonth()];
+  var month = monthNames[(today.getMonth()+parseInt(skip))%12];
   var current_url = EXP_NETFLIX_URL + 'titles-leaving-netflix-in-' + month + '-' + year + '/';
   var query_url = month + '-' + year + '/';
 
   return query_url;
 }
 
-var expFetchCache = {};
-var expFetchingCache = {};
-
-chrome.runtime.sendMessage({contentScriptQuery: "queryExpDates", queryDate: getQueryUrl()},
-    function(response) {
-      expiredMovies = parseResponse(response);
-    }
-);
+for (val in [0, 1]){
+  chrome.runtime.sendMessage({contentScriptQuery: "queryExpDates", queryDate: getQueryUrl(val)},
+      function(response) {
+        expiredMovies = new Map(function*() {
+          yield* expiredMovies; yield* parseResponse(response);
+        }());
+      }
+  );
+}
 
 function getExpiredMovies() {
   return expiredMovies;
 }
-
-// function populateTitles(callback) {
-// 	var cacheKey = hashKey(title);
-// 	if (expFetchedCache[cacheKey]) {
-// 		log("Cached expiration dates for " + cacheKey);
-// 		callback(expFetchedCache[cacheKey]);
-// 	} else if (!expFetchingCache[cacheKey]) {
-// 		log("Fetching expiration dates for " + argsString);
-// 		expfetchingCache[cacheKey] = true;
-// 		$.ajax({
-// 			url: current_url,
-// 			dataType: 'jsonp',
-// 			success: function(response) {
-// 				if (!response.imdbRating && year) {
-// 					log("Failed to fetch ratings for " + argsString);
-// 					delete expFetchingCache[cacheKey];
-// 					return fetchRatings(title, season, episode, null, callback);
-// 				}
-// 				var ratings = {
-// 					imdb: response.imdbRating,
-// 					imdbID: response.imdbID,
-// 					rt: fetchRTRating(response),
-// 					metacritic: response.Metascore
-// 				}
-// 				log("Fetched ratings for " + argsString + ": " + JSON.stringify(ratings));
-// 				fetchedCache[cacheKey] = ratings;
-// 				callback(ratings);
-// 			},
-// 			error: function(jqXHR, status, errorThrown) {
-// 				if (status == "timeout") {
-// 					log("Failed to fetch ratings for " + argsString + " due to timeout");
-// 					delete fetchingCache[cacheKey];
-// 					fetchExpDates(title, season, episode, null, callback);
-// 				}
-// 			},
-// 			timeout: TIMEOUT
-// 		});
-// 	}
-// }
