21b7523bdb40d24c69222f022f2468b94240531e
Tanner Rutgers <rutgers.tanner@gmail.com>
2022-08-15 19:49:10 -0400

    Add new Patreon post

--- app/javascript/background.js
+++ app/javascript/background.js
@@ -12,5 +12,5 @@ chrome.runtime.onMessage.addListener(function(message, sender, response) {
 });
 
 chrome.runtime.onInstalled.addListener(function (object) {
-  chrome.tabs.create({url: "https://www.patreon.com/posts/20865557"})
+  chrome.tabs.create({url: "https://www.patreon.com/posts/rateflix-is-back-70573858"})
 })
