c95acc8c9c1ff7bd18f5d0d3acaa8469fd582d05
Tanner Rutgers <rutgers.tanner@gmail.com>
2022-08-15 19:08:48 -0400

    Fix browsing cards

--- app/javascript/contentscript.js
+++ app/javascript/contentscript.js
@@ -1,154 +1,27 @@
-chrome.runtime.sendMessage({type: 'showPageAction'});
-
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var observerOptions = {
-	childList: true,
-	subtree: true
+function childrenAdded(addedNodes) {
+    for (const node of addedNodes) {
+        if (node.classList.contains("previewModal--wrapper")) {
+            var image_node = node.querySelector('img[alt]:not([alt=""])');
+            var title = image_node.getAttribute('alt');
+            getRatings(title, null, null, null, function(ratings) {
+                var metadata_node = node.querySelector('div.previewModal--metadatAndControls-container');
+                injectRatings(metadata_node, ratings);
+            });
+        }
+    }
 }
 
-var jawBoneContentObserver = new MutationObserver(function(mutations, observer) {
-	var node = mutations.find(function(mutation) { return mutation.target.hasAttribute("observed") });
-	if (node) {
-		node = node.target;
-		var headerNode = node.querySelector(".jawBone > h3");
-		if (headerNode) {
-			var titleNode = headerNode.querySelector(".title");
-			var title = titleNode.querySelector("img") ? titleNode.querySelector("img").alt : titleNode.textContent;
-			if (title) {
-				getRatings(title, null, null, extractYear(node), function(ratings) {
-					injectRatings(node.querySelector(".meta"), ratings);
-				});
-			}
-		}
-	}
+var observer = new MutationObserver(function(mutations, observer) {
+    for (const mutation of mutations) {
+        switch(mutation.type) {
+            case 'childList':
+                childrenAdded(mutation.addedNodes);
+                break;
+        }
+    }
 });
 
-var titleCardObserver = new MutationObserver(function(mutations, observer) {
-	var node = mutations.find(function(mutation) { return mutation.target.hasAttribute("observed") });
-	if (node) {
-		node = node.target;
-		var titleNode = node.querySelector(".bob-title");
-		if (titleNode && (title = titleNode.textContent)) {
-			getRatings(title, null, null, extractYear(node), function(ratings) {
-				injectRatings(node.querySelector(".meta") || titleNode, ratings);
-			});
-		}
-	}
+observer.observe(document.getElementById("appMountPoint"), {
+    childList: true,
+    subtree: true
 });
-
-function addTitleObserver(node) {
-	node.querySelectorAll(".jawBoneContent").forEach(function(node) {
-		if (!node.hasAttribute("observed")) {
-			node.setAttribute("observed", "true");
-			jawBoneContentObserver.observe(node, observerOptions);
-		};
-	});
-	node.querySelectorAll(".title-card-container > div > span").forEach(function(node) {
-		if (!node.hasAttribute("observed")) {
-			node.setAttribute("observed", "true");
-			titleCardObserver.observe(node, observerOptions);
-		};
-	});
-	node.querySelectorAll(".bob-container-tall-panel > span").forEach(function(node) {
-		if (!node.hasAttribute("observed")) {
-			node.setAttribute("observed", "true");
-			titleCardObserver.observe(node, observerOptions);
-		};
-	});
-}
-
-var rowObserver = new MutationObserver(function(mutations, observer) {
-	mutations.forEach(function(mutation) {
-		if (mutation.addedNodes) {
-			mutation.addedNodes.forEach(function(node) {
-				if (node.nodeType === 1) {
-					addTitleObserver(node);
-				}
-			});
-		}
-	});
-});
-
-var mainObserver = new MutationObserver(function(mutations, observer) {
-	var mainView = document.querySelector(".mainView");
-	if (mainView) {
-		observer.disconnect();
-		rowObserver.observe(mainView, observerOptions);
-		addTitleObserver(mainView);
-		addFeaturedRatings(mainView);
-	}
-});
-
-function addFeaturedRatings(node) {
-	var jawBoneNode = node.querySelector(".jawBoneContainer > .jawBone");
-	if (jawBoneNode) {
-		var titleNode = jawBoneNode.querySelector(".title");
-		if (titleNode) {
-			if (img = titleNode.querySelector("img")) {
-				title = img.alt;
-			} else {
-				title = titleNode.textContent;
-			}
-			getRatings(title, null, null, extractYear(jawBoneNode), function(ratings) {
-				injectRatings(node.querySelector(".meta"), ratings);
-			});
-		}
-	}
-}
-
-var playerObserver = new MutationObserver(function(mutations, observer) {
-	if (titleContainerNode = document.querySelector(".video-title")) {
-		observer.disconnect();
-		addPlayerRatings(titleContainerNode);
-	}
-});
-
-function addPlayerRatings(titleContainerNode) {
-	var titleNode = titleContainerNode.getElementsByTagName('h4')[0];
-	var episodeInfo = {};
-	Array.prototype.some.call(titleContainerNode.getElementsByTagName('span'), function(span) {
-		if (span.classList.length == 0) {
-			episodeInfo = extractEpisodeInfo(span.textContent);
-			return true;
-		}
-	});
-	getRatings(titleNode.textContent, episodeInfo["season"], episodeInfo["episode"], null, function(ratings) {
-		injectRatings(titleNode.parentNode, ratings);
-	});
-}
-
-var episodeContainerObserver = new MutationObserver(function(mutations, observer) {
-	var episodeListContainer = document.querySelector(".episodes-pane");
-	if (episodeListContainer) {
-		addEpisodeRatings(episodeListContainer);
-	}
-});
-
-function addEpisodeRatings(episodeListContainer) {
-	var title = document.querySelector(".video-title").getElementsByTagName('h4')[0].textContent;
-	var seasonNode = episodeListContainer.querySelector(".header-title");
-	var season = extractSeasonNumber(seasonNode.textContent);
-	if (season) {
-		var episodes = episodeListContainer.querySelectorAll(".episode-row > div > span.number");
-		episodes.forEach(function(episode) {
-			if (title) {
-				getRatings(title, season, episode.textContent, null, function(ratings) {
-					injectRatings(episode.parentNode, ratings);
-				});
-			}
-		});
-	}
-}
-
-if (mainView = document.querySelector(".mainView")) {
-	rowObserver.observe(mainView, observerOptions);
-	addTitleObserver(mainView);
-	addFeaturedRatings(mainView);
-} else if (titleContainerNode = document.querySelector(".video-title")) {
-	addPlayerRatings(titleContainerNode);
-} else {
-	mainObserver.observe(document, observerOptions);
-	playerObserver.observe(document, observerOptions);
-	episodeContainerObserver.observe(document, observerOptions);
-}
--- app/javascript/inject.js
+++ app/javascript/inject.js
@@ -1,111 +1,91 @@
-function imdbSpan() {
-	var span = document.createElement("SPAN");
-	span.className = "imdbRating";
-	return span;
-}
-
-function rtSpan() {
-	var span = document.createElement("SPAN");
-	span.className = "rtRating";
-	return span;
-}
-
-function metacriticSpan() {
-	var span = document.createElement("SPAN");
-	span.className = "metacriticRating";
-	return span;
-}
-
-function imdbLinkNode(id) {
-	var link = document.createElement("A");
+function imdbLink(id) {
+	var link = document.createElement("a");
 	link.href = "https://www.imdb.com/title/" + id;
 	link.target = "_blank";
+	link.id = "imdbRating";
 	return link;
 }
 
-function imdbLogoNode(id) {
-	var span = imdbSpan();
-	var link = imdbLinkNode(id);
-	var image = document.createElement("IMG");
-	image.src = chrome.extension.getURL("images/imdb_31x14.png");
-	image.className = "imdbLogo";
-	link.appendChild(image);
-	span.appendChild(link);
-	return span;
-}
+function injectImdbRating(ratingsDiv, ratings) {
+	var rating = ratings["imdb"];
+	var id = ratings["imdbID"];
 
-function imdbRatingNode(id, rating) {
-	rating = rating.replace("N/A", "");
-	var span = imdbSpan();
-	var link = imdbLinkNode(id);
-	var rating = document.createTextNode(rating);
-	link.appendChild(rating);
-	span.appendChild(link);
-	return span;
-}
-
-function rtLogoNode() {
-	var span = rtSpan();
-	var image = document.createElement("IMG");
-	image.src = chrome.extension.getURL("images/rt_logo.png");
-	image.className = "rtLogo";
-	span.appendChild(image);
-	return span;
-}
-
-function rtRatingNode(rating) {
-	var span = rtSpan();
-	var rating = document.createTextNode(rating);
-	span.appendChild(rating);
-	return span;
-}
-
-function metacriticLogoNode() {
-	var span = metacriticSpan();
-	var image = document.createElement("IMG");
-	image.src = chrome.extension.getURL("images/metacritic_logo.png");
-	image.className = "metacriticLogo";
-	span.appendChild(image);
-	return span;
-}
-
-function metacriticRatingNode(rating) {
-	var span = metacriticSpan();
-	var rating = document.createTextNode(rating);
-	span.appendChild(rating);
-	return span;
-}
-
-function should_append_imdb(rating, id) {
 	if ((rating && rating != "N/A") || id) {
-		return true;
+		var logo = document.createElement("img");
+		logo.src = chrome.extension.getURL("images/imdb_31x14.png");
+		logo.className = "imdbLogo";
+		logo.style.paddingRight = "0.5vw";
+
+		var span = document.createElement("span");
+		span.appendChild(logo);
+
+		if (rating && rating != "N/A") {
+			var rating = document.createTextNode(rating);
+			span.appendChild(rating);
+		}
+
+		if (id) {
+			link = imdbLink(id);
+			link.style.paddingRight = "1vw";
+			link.appendChild(span);
+			ratingsDiv.appendChild(link);
+		} else {
+			span.style.paddingRight = "1vw";
+			ratingsDiv.appendChild(span);
+		}
 	}
-	return false;
+}
+
+function injectRtRating(ratingsDiv, ratings) {
+	var rating = ratings["rt"];
+
+	if (rating && rating != "N/A") {
+		var logo = document.createElement("img");
+		logo.src = chrome.extension.getURL("images/rt_logo.png");
+		logo.className = "rtLogo";
+		logo.style.paddingRight = "0.5vw";
+
+		var rating = document.createTextNode(rating);
+
+		var span = document.createElement("span");
+		span.style.paddingRight = "1vw";
+		span.appendChild(logo);
+		span.appendChild(rating);
+		
+		ratingsDiv.appendChild(span);
+	}
+}
+
+function injectMetascoreRating(ratingsDiv, ratings) {
+	var rating = ratings["metacritic"];
+
+	if (rating && rating != "N/A") {
+		var logo = document.createElement("img");
+		logo.src = chrome.extension.getURL("images/metacritic_logo.png")
+		logo.className = "metacriticLogo";
+		logo.style.paddingRight = "0.5vw";
+
+		var rating = document.createTextNode(rating);
+
+		var span = document.createElement("span");
+		span.style.paddingRight = "1vw";
+		span.appendChild(logo);
+		span.appendChild(rating);
+		
+		ratingsDiv.appendChild(span);
+	}
 }
 
 function injectRatings(node, ratings) {
-	var imdbRating = ratings["imdb"];
-	var imdbId = ratings["imdbID"];
-	var rtRating = ratings["rt"];
-	var metascore = ratings["metacritic"];
+	if (!node.querySelector("#rateFlixRatings")) {
+		ratingsDiv = document.createElement("div");
+		ratingsDiv.style.opacity = 1;
+		ratingsDiv.id = "rateFlixRatings";
 
-	if (node) {
-		if (!node.querySelector(".imdbRating")) {
-			if (should_append_imdb(imdbRating, imdbId)) {
-				node.appendChild(imdbLogoNode(imdbId));
-				node.appendChild(imdbRatingNode(imdbId, imdbRating));
-			} else {
-				node.appendChild(imdbSpan());
-				node.appendChild(imdbSpan());
-			}
-		}
-		if (rtRating && !node.querySelector(".rtRating")) {
-			node.appendChild(rtLogoNode());
-			node.appendChild(rtRatingNode(rtRating));
-		}
-		if (metascore && metascore != "N/A" && !node.querySelector(".metacriticRating")) {
-			node.appendChild(metacriticLogoNode());
-			node.appendChild(metacriticRatingNode(metascore));
-		}
+		injectImdbRating(ratingsDiv, ratings);
+		injectRtRating(ratingsDiv, ratings);
+		injectMetascoreRating(ratingsDiv, ratings);
+
+		node.appendChild(ratingsDiv);
 	}
 }
