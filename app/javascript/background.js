f76ebc7fd07e45f43f9c860b80b919f18d52b248
Tanner Rutgers <rutgers.tanner@gmail.com>
2022-08-15 20:26:44 -0400

    Remove unnecessary perms

--- app/javascript/background.js
+++ app/javascript/background.js
@@ -1,10 +1,3 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (/netflix\.com/.test(changeInfo.url)) {
-		console.log("tab updated, reloading script");
-		chrome.tabs.executeScript(null, { file: "javascript/contentscript.js" });
-	}
-});
-
 chrome.runtime.onMessage.addListener(function(message, sender, response) {
 	if (message.type == 'showPageAction') {
 		chrome.pageAction.show(sender.tab.id);
