317126f564325587ac530ada28afe3908e242d61
Tanner Rutgers <rutgers.tanner@gmail.com>
2022-10-11 09:36:50 -0400

    Fix DRY inject

--- app/javascript/inject.js
+++ app/javascript/inject.js
@@ -1,25 +1,34 @@
 function imdbLink(id) {
-	var link = document.createElement("a");
-	link.href = "https://www.imdb.com/title/" + id;
-	link.target = "_blank";
-	link.id = "rateflix--imdbRating";
-	return link;
+	if (id) {
+		var link = document.createElement("a");
+		link.href = "https://www.imdb.com/title/" + id;
+		link.target = "_blank";
+		link.id = "rateflix--imdbRating";
+		return link;
+	}
 }
 
-function injectRating(rating, ratingsDiv, icon) {
-	if (rating && rating != "N/A") {
+function injectRating(ratingsDiv, rating, icon_location, link) {
+	if ((rating && rating != "N/A") || link) {
 		var icon = document.createElement("img");
-		icon.src = chrome.runtime.getURL(icon)
+		icon.src = chrome.runtime.getURL(icon_location);
 		icon.className = "rateflix--icon";
-
-		var rating = document.createTextNode(rating);
 
 		var span = document.createElement("span");
 		span.className = "rateflix--ratings-span"
 		span.appendChild(icon);
-		span.appendChild(rating);
-		
-		ratingsDiv.appendChild(span);
+
+		if (rating && rating != "N/A") {
+			var rating = document.createTextNode(rating);
+			span.appendChild(rating);
+		}
+
+		if (link) {
+			link.appendChild(span);
+			ratingsDiv.appendChild(link);
+		} else {
+			ratingsDiv.appendChild(span);
+		}
 	}
 }
 
@@ -29,9 +38,9 @@ function injectRatings(node, ratings) {
 		ratingsDiv.style.opacity = 1;
 		ratingsDiv.id = "rateflix--ratings";
 
-		injectRating(ratings["imdb"], ratingsDiv, "images/imdb_31x14.png")
-		injectRating(ratings["rt"], ratingsDiv, "images/rt_logo.png")
-		injectRating(ratings["metacritic"], ratingsDiv, "images/metacritic_logo.png")
+		injectRating(ratingsDiv, ratings["imdb"], "images/imdb_31x14.png", imdbLink(ratings["imdbId"]))
+		injectRating(ratingsDiv, ratings["rt"], "images/rt_logo.png", null)
+		injectRating(ratingsDiv, ratings["metacritic"], "images/metacritic_logo.png", null)
 
 		node.appendChild(ratingsDiv);
 	}
