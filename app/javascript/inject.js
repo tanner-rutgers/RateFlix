c0dc79b560efe50cbc92f91062a8600ccdd75e73
Tanner Rutgers <rutgers.tanner@gmail.com>
2022-10-11 09:09:13 -0400

    DRY inject

--- app/javascript/inject.js
+++ app/javascript/inject.js
@@ -6,66 +6,17 @@ function imdbLink(id) {
 	return link;
 }
 
-function injectImdbRating(ratingsDiv, ratings) {
-	var rating = ratings["imdb"];
-	var id = ratings["imdbID"];
-
-	if ((rating && rating != "N/A") || id) {
-		var logo = document.createElement("img");
-		logo.src = chrome.runtime.getURL("images/imdb_31x14.png");
-		logo.className = "rateflix--logo";
-
-		var span = document.createElement("span");
-		span.className = "rateflix--ratings-span"
-		span.appendChild(logo);
-
-		if (rating && rating != "N/A") {
-			var rating = document.createTextNode(rating);
-			span.appendChild(rating);
-		}
-
-		if (id) {
-			link = imdbLink(id);
-			link.appendChild(span);
-			ratingsDiv.appendChild(link);
-		} else {
-			ratingsDiv.appendChild(span);
-		}
-	}
-}
-
-function injectRtRating(ratingsDiv, ratings) {
-	var rating = ratings["rt"];
-
+function injectRating(rating, ratingsDiv, icon) {
 	if (rating && rating != "N/A") {
-		var logo = document.createElement("img");
-		logo.src = chrome.runtime.getURL("images/rt_logo.png");
-		logo.className = "rateflix--logo";
+		var icon = document.createElement("img");
+		icon.src = chrome.runtime.getURL(icon)
+		icon.className = "rateflix--icon";
 
 		var rating = document.createTextNode(rating);
 
 		var span = document.createElement("span");
 		span.className = "rateflix--ratings-span"
-		span.appendChild(logo);
-		span.appendChild(rating);
-		
-		ratingsDiv.appendChild(span);
-	}
-}
-
-function injectMetascoreRating(ratingsDiv, ratings) {
-	var rating = ratings["metacritic"];
-
-	if (rating && rating != "N/A") {
-		var logo = document.createElement("img");
-		logo.src = chrome.runtime.getURL("images/metacritic_logo.png")
-		logo.className = "rateflix--logo";
-
-		var rating = document.createTextNode(rating);
-
-		var span = document.createElement("span");
-		span.className = "rateflix--ratings-span"
-		span.appendChild(logo);
+		span.appendChild(icon);
 		span.appendChild(rating);
 		
 		ratingsDiv.appendChild(span);
@@ -78,9 +29,9 @@ function injectRatings(node, ratings) {
 		ratingsDiv.style.opacity = 1;
 		ratingsDiv.id = "rateflix--ratings";
 
-		injectImdbRating(ratingsDiv, ratings);
-		injectRtRating(ratingsDiv, ratings);
-		injectMetascoreRating(ratingsDiv, ratings);
+		injectRating(ratings["imdb"], ratingsDiv, "images/imdb_31x14.png")
+		injectRating(ratings["rt"], ratingsDiv, "images/rt_logo.png")
+		injectRating(ratings["metacritic"], ratingsDiv, "images/metacritic_logo.png")
 
 		node.appendChild(ratingsDiv);
 	}
